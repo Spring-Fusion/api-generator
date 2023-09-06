43ed886bdb9c28f85151c3e12ec343365faaecaf
Diogo Ribeiro <diogoribeiro698@gmail.com>
2023-09-06 22:42:34 +0000

    add: swagger to document

--- src/main/java/com/appfusion/apigenerator/builder/controller/EntityController.java
+++ src/main/java/com/appfusion/apigenerator/builder/controller/EntityController.java
@@ -12,8 +12,14 @@ import com.appfusion.apigenerator.builder.entities.PostEntity;
 import com.appfusion.apigenerator.builder.resourceLoader.ResourceLoader;
 import com.appfusion.apigenerator.builder.service.PostEntityService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/entity")
+@Tag(name = "api-generator")
 public class EntityController {
 
   @Autowired
@@ -23,26 +29,46 @@ public class EntityController {
     this.service = service;
   }
 
+  @Operation(summary = "Save an entity")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "success")
+  })
   @PostMapping("/saveEntity")
   public ResponseEntity<PostEntity> saveEntityReq(@RequestBody String entity) {
     return service.saveEntity(entity);
   }
 
+  @Operation(summary = "Create an entity")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "success")
+  })
   @PostMapping("/createEntity/{id}")
   public void generatePostEntity(@PathVariable Long id) throws Exception {
     service.generateEntity(id);
   }
 
+  @Operation(summary = "Generate a project")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "success")
+  })
   @PostMapping("/generateProject")
   public void generateProject(@RequestBody String clientId) throws Exception {
     ResourceLoader.generateNewProject(clientId);
   }
 
+  @Operation(summary = "Publish a project")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "success")
+  })
   @PostMapping("/publishProject")
   public void publishProject(@RequestBody String clientId) throws Exception{
     ResourceLoader.publishProject(clientId);
   }
 
+  @Operation(summary = "Clean a project content")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "success")
+  })
   @PostMapping("/cleanProjectContent")
   public void cleanProjectContent(@RequestBody String clientId) throws Exception{
     ResourceLoader.cleanProjectContent(clientId);
