663e982f13f663730885f38b45f3b7c00ea21cac
Gabriel Reis <tharazo101@gmail.com>
2023-11-09 08:57:57 -0300

    doc: Templates

--- src/main/java/com/appfusion/apigenerator/builder/templates/ControllerTemplate.java
+++ src/main/java/com/appfusion/apigenerator/builder/templates/ControllerTemplate.java
@@ -13,6 +13,16 @@ import com.squareup.javapoet.ClassName;
 
 import lombok.Data;
 
+/**
+ * This class represents a template for generating a Spring Boot controller
+ * class.
+ * It contains fields for the necessary class names and a constructor to
+ * initialize them.
+ * It also provides a static method to create a new instance of the template
+ * based on a JSON string.
+ *
+ * @author Gabriel Reis
+ */
 @Data
 public class ControllerTemplate {
 
@@ -27,31 +37,30 @@ public class ControllerTemplate {
   private ClassName updateByIdClass;
 
   public ControllerTemplate(ClassName restController, ClassName autoWired, ClassName runTimeRepositoryClass,
-      ClassName requestEndPoint, ClassName getEndPoint, ClassName deleteEdnPoint, ClassName requestBodyClass, ClassName runTimeEntityClass,
-      ClassName updateByIdClass
-      ) {
-      this.restController = restController;
-      this.autoWired = autoWired;
-      this.runTimeRepositoryClass = runTimeRepositoryClass;
-      this.requestEndPoint = requestEndPoint;
-      this.getEndPoint = getEndPoint;
-      this.deleteEndPoint = deleteEdnPoint;
-      this.requestBodyClass = requestBodyClass;
-      this.runTimeEntityClass = runTimeEntityClass;
-      this.updateByIdClass = updateByIdClass;
+      ClassName requestEndPoint, ClassName getEndPoint, ClassName deleteEdnPoint, ClassName requestBodyClass,
+      ClassName runTimeEntityClass,
+      ClassName updateByIdClass) {
+    this.restController = restController;
+    this.autoWired = autoWired;
+    this.runTimeRepositoryClass = runTimeRepositoryClass;
+    this.requestEndPoint = requestEndPoint;
+    this.getEndPoint = getEndPoint;
+    this.deleteEndPoint = deleteEdnPoint;
+    this.requestBodyClass = requestBodyClass;
+    this.runTimeEntityClass = runTimeEntityClass;
+    this.updateByIdClass = updateByIdClass;
   }
 
   public static ControllerTemplate getControllerTemplate(String json) {
     return new ControllerTemplate(
-      new RestControllerClassName().getContent(),
-      new AutoWiredClassName().getContent(),
-      new RunTimeRepositoryClassName(json).getContent(),
-      new RequestEndPointClassName().getContent(),
-      new GetEndPontClassName().getContent(),
-      new DeleteEndPointClassName().getContent(),
-      new RequestBodyClassName().getContent(),
-      new RunTimeEntityClassName(json).getContent(),
-      new UpdateByIdClassName().getContent()
-      );
+        new RestControllerClassName().getContent(),
+        new AutoWiredClassName().getContent(),
+        new RunTimeRepositoryClassName(json).getContent(),
+        new RequestEndPointClassName().getContent(),
+        new GetEndPontClassName().getContent(),
+        new DeleteEndPointClassName().getContent(),
+        new RequestBodyClassName().getContent(),
+        new RunTimeEntityClassName(json).getContent(),
+        new UpdateByIdClassName().getContent());
   }
 }
