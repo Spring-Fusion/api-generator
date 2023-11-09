3e603d369f2d943bcaa6863604ef3b30eb1234be
Gabriel Reis <tharazo101@gmail.com>
2023-11-09 16:31:49 -0300

    doc: Factory

--- src/main/java/com/appfusion/apigenerator/builder/factory/FieldFactory.java
+++ src/main/java/com/appfusion/apigenerator/builder/factory/FieldFactory.java
@@ -6,20 +6,25 @@ import com.appfusion.apigenerator.builder.DTOs.EntityDTO;
 import com.appfusion.apigenerator.builder.templates.ModelTemplate;
 import com.squareup.javapoet.FieldSpec;
 
+/**
+ * This class provides factory methods for creating FieldSpec objects.
+ * 
+ * @author Gabriel Reis
+ */
 public class FieldFactory {
 
   public static FieldSpec getRepositoryField(EntityDTO dto) {
     return FieldSpec
-      .builder(dto.getTemplate().getRunTimeRepositoryClass(), "repository", Modifier.PRIVATE)
-      .addAnnotation(AnnotationFactory.getAutoWiredAnnotation(dto))
-      .build();
+        .builder(dto.getTemplate().getRunTimeRepositoryClass(), "repository", Modifier.PRIVATE)
+        .addAnnotation(AnnotationFactory.getAutoWiredAnnotation(dto))
+        .build();
   }
 
   public static FieldSpec getIdFieldSpec(ModelTemplate postTemplate) {
     return FieldSpec
-      .builder(Long.class, "id")
-      .addModifiers(Modifier.PRIVATE).addAnnotation(AnnotationFactory.getIdPostIdAnnotation(postTemplate))
-      .addAnnotation(AnnotationFactory.getGeneratedValueAnnotation(postTemplate))
-      .build();
+        .builder(Long.class, "id")
+        .addModifiers(Modifier.PRIVATE).addAnnotation(AnnotationFactory.getIdPostIdAnnotation(postTemplate))
+        .addAnnotation(AnnotationFactory.getGeneratedValueAnnotation(postTemplate))
+        .build();
   }
 }
